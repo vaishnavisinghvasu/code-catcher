d5079521b52b55f31a018102752a6b97a5ff7d00
vaishnavivasusingh@gmail.com <vaishnavivasusingh@gmail.com>
2025-09-25 17:42:02 +0530

    Fix download route with app.root_path

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+from flask import Flask, render_template, send_file
 import os
-from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/download")
+def download_game():
+    zip_path = os.path.join(app.root_path, "Code-Catcher-Game.zip")
+    return send_file(zip_path, as_attachment=True)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
